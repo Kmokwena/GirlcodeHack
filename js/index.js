607749fdacceb038a531847aa7b401464f5e0199
kmokwena <mokwenakw@gmail.com>
2022-08-07 00:47:27 +0200

    form validation done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,46 @@
-const password = document.getElementById("surname").value;
-const confirmPass = document.getElementById("confirmpassword").value;
 const errorMsg = document.getElementById("errorMsgs");
-
-let email = document.getElementById("email");
 function isRegValid() {
-  console.log(password + " , " + confirmPass);
-
-  //errorMsg.innerHTML = "Your passwords do not match";
+  const password = document.getElementById("password").value;
+  const confirmPass = document.getElementById("confirmpassword").value;
+  const idNumber = document.getElementById("idNum").value;
+  const username = document.getElementById("name").value;
+  const surname = document.getElementById("surname").value;
+  const email = document.getElementById("email").value;
+  const cellphone = document.getElementById("cellphone").value;
+  if (
+    password === "" ||
+    idNumber === "" ||
+    username === "" ||
+    surname === "" ||
+    password === "" ||
+    password === null
+  ) {
+    errorMsg.innerHTML = "Enter all fields";
+  }
+  if (email === "" && cellphone === "") {
+    errorMsg.innerHTML = "Please enter your email or cellphone number.";
+  }
+  if (password != confirmPass) {
+    errorMsg.innerHTML = "Your passwords do not match";
+  }
+  if (idNumber.match(/[\/D]/) === false || idNumber.length > 13) {
+    errorMsg.innerHTML = "Your ID number is invalid.";
+  }
+  if (
+    idNumber[7] === "5" ||
+    idNumber[7] === "6" ||
+    idNumber[7] === "7" ||
+    idNumber[7] === "8" ||
+    idNumber[7] === "9"
+  ) {
+    errorMsg.innerHTML = "You are not a female.";
+  }
+  if (password.length < 6) {
+    errorMsg.innerHTML = "Password should be more than 5 characters long.";
+  }
+  if (/[\/d\/w]/.test(password) === false) {
+    errorMsg.innerHTML = "Password should include letters and number.";
+  }
 }
 
 function isLoginValid() {
