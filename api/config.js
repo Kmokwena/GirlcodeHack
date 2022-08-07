80a5799aedd8df73e14965cc67c52c280e3d9396
mamaleka <damentt@github.com>
2022-08-07 07:00:05 +0200

    block the current user from send invalid data

--- api/config.js
+++ api/config.js
@@ -8,10 +8,12 @@ const client = new Client({
   port: process.env.DB_PORT || 5432,
 });
 
+client.connect()
+
 async function createTable() {
   try {
     const res = await client.query(
-      ` CREATE TABLE [IF NOT EXISTS] users(id SERIAL PRIMARY KEY, Name varchar NOT NULL, Surname VARCHAR NOT NULL, email VARCHAR, cellphone VARCHAR, idno VARCHAR NOT NULL, password VARCHAR NOT NULL)`
+      ` CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, Name varchar NOT NULL, Surname VARCHAR NOT NULL, email VARCHAR, cellphone VARCHAR, idno VARCHAR NOT NULL, password VARCHAR NOT NULL)`
     );
     return res;
   } catch (err) {
--- public/js/index.js
+++ public/js/index.js
@@ -18,39 +18,42 @@ function isRegValid() {
     password === null
   ) {
     errorMsg.innerHTML = "Enter all fields";
+    return false
   }
   if (email === "" && cellphone === "") {
     errorMsg.innerHTML = "Please enter your email or cellphone number.";
+    return false
   }
   if (password != confirmPass) {
     errorMsg.innerHTML = "Your passwords do not match";
+    return false
   }
-  if (idNumber.match(/[\/D]/) === false || idNumber.length > 13) {
+  if (idNumber.match(/\D/)  || idNumber.length != 13) {
     errorMsg.innerHTML = "Your ID number is invalid.";
+    return false
   }
   if (
-    idNumber[7] === "5" ||
-    idNumber[7] === "6" ||
-    idNumber[7] === "7" ||
-    idNumber[7] === "8" ||
-    idNumber[7] === "9"
-  ) {
+    idNumber[7] >= 5 && idNumber[7] <= 9  ) {
     errorMsg.innerHTML = "You are not a female.";
+    return false
   }
   if (password.length < 6) {
     errorMsg.innerHTML = "Password should be more than 5 characters long.";
+    return false
   }
-  if (/[\/d\/w]/.test(password) === false) {
+  if (/\w/.test(password) === false || /\d/.test(password) === false) {
     errorMsg.innerHTML = "Password should include letters and number.";
+    return false
   }
 
   axios.post('/users/register', userData)
   .then(function (response) {
-    console.log(response);
+    window.location.replace("/welcome.html") //redirect here
   })
   .catch(function (error) {
-    console.log(error);
+    errorMsg.innerHTML = "registration failed";
   })
+  return true;
 }
 
 function isLoginValid() {
